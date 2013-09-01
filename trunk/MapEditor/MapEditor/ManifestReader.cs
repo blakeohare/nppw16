203e0e6741232e13679d25e5aa5330eb85517b63
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-01 00:58:09 +0000

    First "real" checkin.
    Stubs.
    Test tiles.
    Map editor sapling.

---
+++ trunk/MapEditor/MapEditor/ManifestReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapEditor
+{
+	public class ManifestReader
+	{
+
+		public static void Initialize()
+		{
+			string tileDirectory = FindTileDirectory();
+			
+		}
+
+		// This is a horrible horrible hack and that's okay.
+		private static string FindTileDirectory()
+		{
+			string currentDirectory = System.IO.Path.GetFullPath(System.Environment.CurrentDirectory);
+			string svnRoot = null;
+			while (svnRoot == null)
+			{
+				string checkForThis = System.IO.Path.Combine(currentDirectory, "License.txt");
+				if (System.IO.File.Exists(checkForThis))
+				{
+					svnRoot = currentDirectory;
+				}
+				else
+				{
+					currentDirectory = System.IO.Path.GetDirectoryName(currentDirectory);
+				}
+			}
+
+			return System.IO.Path.Combine(svnRoot, "images", "tiles");
+		}
+	}
+}
