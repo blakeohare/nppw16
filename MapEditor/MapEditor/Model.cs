04f69b59083fbd5cce3bce75a21895ffab0dd6e8
Blake O'Hare <blakeohare@users.noreply.github.com>
2013-09-01 04:16:33 +0000

    More map editor stuff.

---
+++ MapEditor/MapEditor/Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapEditor
+{
+	public  class Model
+	{
+		public bool IsDirty { get; set; }
+
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		// High frequency access, field that behaves like a property for performance.
+		public TileTemplate[] TilesUpper;
+		public TileTemplate[] TilesLower;
+
+
+		public Model(int width, int height)
+		{
+			this.Width = width;
+			this.Height = height;
+			this.IsDirty = true;
+		}
+
+		public string Serialize()
+		{
+			this.values["width"] = "" + this.Width;
+			this.values["height"] = "" + this.Height;
+			this.values["upper"] = this.SerializeTiles(this.TilesUpper);
+			this.values["lower"] = this.SerializeTiles(this.TilesLower);
+
+			List<string> output = new List<string>();
+			foreach (string key in this.values.Keys)
+			{
+				output.Add("#" + key + ":" + this.values[key]);
+			}
+
+			return string.Join("\n", output);
+		}
+
+		private string SerializeTiles(IList<TileTemplate> tiles)
+		{
+			return string.Join(",", tiles.Select<TileTemplate, string>(tile => tile.ID));
+		}
+
+		public void SetTiles(TileTemplate[] tiles, bool upperLayer)
+		{
+			if (upperLayer)
+			{
+				this.TilesUpper = tiles;
+			}
+			else
+			{
+				this.TilesLower = tiles;
+			}
+		}
+
+		private Dictionary<string, string> values;
+		public void SetRawFileData(Dictionary<string, string> values)
+		{
+			this.values = values;
+		}
+	}
+}
