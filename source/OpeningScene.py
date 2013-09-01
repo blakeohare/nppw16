755b254ef102c2cc1f9a7307d10d278d768d717c
Blake O'Hare <blakeohare@users.noreply.github.com>
2013-09-01 21:26:47 +0000

    Render Text

--- source/OpeningScene.py
+++ source/OpeningScene.py
@@ -1,53 +1,60 @@
-
+GO_TO_TITLE = True
 
 class OpeningScene:
 	def __init__(self):
-		self.mode = 'open'
-		self.ch3 = [
-			' XXX  X  X   XXX ',
-			'XX XX X  X  X   X',
-			'X     X  X      X',
-			'X     XXXX    XX ',
-			'X     X  X      X',
-			'XX XX X  X  X   X',
-			' XXX  X  X   XXX '
+		if GO_TO_TITLE:
+			self.next = TitleScene()
+			self.mode = None
+			self.flags = ''
+			self.fade = 0
+			self.mode_counter = 0
+		else:
+			self.mode = 'open'
+			self.ch3 = [
+				' XXX  X  X   XXX ',
+				'XX XX X  X  X   X',
+				'X     X  X      X',
+				'X     XXXX    XX ',
+				'X     X  X      X',
+				'XX XX X  X  X   X',
+				' XXX  X  X   XXX '
+				]
+			self.logo = [
+				'TTTTT TTTTT  TTT  T     T       N   N NNNN    NNNN  NNNN  NNNNN  NNN  NNNNN N   N NNNNN  NNN ',
+				'  T   T     T   T TT   TT       NN  N N   N   N   N N   N N     N   N N     NN  N   N   N   N',
+				'  T   T     T   T T T T T       N N N N   N   N   N N   N N     N     N     N N N   N   N    ',
+				'  T   TTT   TTTTT T  T  T       N  NN NNNN    NNNN  NNNN  NNN    NNN  NNN   N  NN   N    NNN ',
+				'  T   T     T   T T     T       N   N N       N     N N   N         N N     N   N   N       N',
+				'  T   T     T   T T     T       N   N N       N     N  N  N     N   N N     N   N   N   N   N',
+				'  T   TTTTT T   T T     T       N   N N       N     N   N NNNNN  NNN  NNNNN N   N   N    NNN ',
 			]
-		self.logo = [
-			'TTTTT TTTTT  TTT  T     T       N   N NNNN    NNNN  NNNN  NNNNN  NNN  NNNNN N   N NNNNN  NNN ',
-			'  T   T     T   T TT   TT       NN  N N   N   N   N N   N N     N   N N     NN  N   N   N   N',
-			'  T   T     T   T T T T T       N N N N   N   N   N N   N N     N     N     N N N   N   N    ',
-			'  T   TTT   TTTTT T  T  T       N  NN NNNN    NNNN  NNNN  NNN    NNN  NNN   N  NN   N    NNN ',
-			'  T   T     T   T T     T       N   N N       N     N N   N         N N     N   N   N       N',
-			'  T   T     T   T T     T       N   N N       N     N  N  N     N   N N     N   N   N   N   N',
-			'  T   TTTTT T   T T     T       N   N N       N     N   N NNNNN  NNN  NNNNN N   N   N    NNN ',
-		]
-		self.next = self
-		self.flags = 'W'
-		static = pygame.Surface((120, 120))
-		self.scrolly = pygame.Surface((WIDTH, 5))
-		black = (0, 0, 0)
-		white = (255, 255, 255)
-		self.dark = pygame.Surface((WIDTH, HEIGHT)).convert()
-		self.dark.fill((0, 0, 0))
+			self.next = self
+			self.flags = 'W'
+			static = pygame.Surface((120, 120))
+			self.scrolly = pygame.Surface((WIDTH, 5))
+			black = (0, 0, 0)
+			white = (255, 255, 255)
+			self.dark = pygame.Surface((WIDTH, HEIGHT)).convert()
+			self.dark.fill((0, 0, 0))
+			
+			dr = pygame.draw.rect
+			pr = pygame.Rect
+			e = 0
+			y = 0
+			self.fade = 0
+			colors = [black, white] * (120 * 60)
+			random.shuffle(colors)
+			i = 0
+			while y < 120:
+				x = 0
+				while x < 120:
+					dr(static, colors[i], pr(x, y, 2, 2))
+					i += 1
+					x += 1
+				y += 1
+			self.static = static
+			self.mode_counter = 0
 		
-		dr = pygame.draw.rect
-		pr = pygame.Rect
-		e = 0
-		y = 0
-		self.fade = 0
-		colors = [black, white] * (120 * 60)
-		random.shuffle(colors)
-		i = 0
-		while y < 120:
-			x = 0
-			while x < 120:
-				dr(static, colors[i], pr(x, y, 2, 2))
-				i += 1
-				x += 1
-			y += 1
-		self.static = static
-		self.mode_counter = 0
-	
 	def processInput(self, events, pressedKeys):
 		for event in events:
 			
