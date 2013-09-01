68f87a0eb550dc7e6d5fe66c85d0580afc6b3c71
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-01 22:31:41 +0000

    Some intro stuff and image loading.

--- trunk/source/TitleScene.py
+++ trunk/source/TitleScene.py
@@ -2,12 +2,18 @@ class TitleScene:
 	def __init__(self):
 		self.next = self
 		self.flags = ''
+		self.flash_counter = 0
 	
 	def processInput(self, events, pressedKeys):
-		pass
+		for event in events:
+			if event.down and event.action == 'start':
+				self.flash_counter = 30
+				# TODO: play sound
 	
 	def update(self):
-		pass
+		self.flash_counter -= 1
+		if self.flash_counter == 1:
+			self.next = IntroScene()
 	
 	def render(self, screen, renderCounter):
 		screen.fill((0, 0, 0))
@@ -15,3 +21,15 @@ class TitleScene:
 		
 		img = renderText((255, 255, 255), "Hello, World!")
 		screen.blit(img, (100, 100))
+		
+		screen.fill((0, 0, 0))
+		screen.blit(getImage('slides/title.png'), (0, 0))
+		
+		showText = (renderCounter // 10) % 3 != 0
+		if self.flash_counter > 0:
+			showText = renderCounter % 4 < 2
+		if showText:
+			pressStart = renderText((255, 255, 255), "< Press Start >")
+			
+			x = (screen.get_width() - pressStart.get_width()) // 2
+			screen.blit(pressStart, (x, 140))
