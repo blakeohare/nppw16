fc04da70f55c0448db005ac24c45c12cbb71a951
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-01 21:26:47 +0000

    Render Text

--- trunk/source/TitleScene.py
+++ trunk/source/TitleScene.py
@@ -12,3 +12,6 @@ class TitleScene:
 	def render(self, screen, renderCounter):
 		screen.fill((0, 0, 0))
 		pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(10, 10, 50, 50))
+		
+		img = renderText((255, 255, 255), "Hello, World!")
+		screen.blit(img, (100, 100))
