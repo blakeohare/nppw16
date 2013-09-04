13842d090c4a8f3064036de426255aee95a1ee83
Blake O'Hare <blakeohare@users.noreply.github.com>
2013-09-04 19:37:20 +0000

    Move enemy automation into their own files.

---
+++ source/EnemyAutomation/AcornTopAutomation.py
@@ -0,0 +1,18 @@
+
+class AcornTopAutomation:
+	def __init__(self, sprite):
+		self.sprite = sprite
+		self.countdown = 40
+	
+	def doStuff(self, scene):
+		self.countdown -= 1
+		if self.countdown == 0:
+			self.goLeft = not self.goLeft
+		
+		# TODO: "loop" as it approaches 0 and then afterwards, go back to the sender in a straight line
+		if self.goLeft:
+			self.sprite.dx = -3
+		else:
+			self.sprite.dx = 3
+		
+				
