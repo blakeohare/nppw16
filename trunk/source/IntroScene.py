609925756f74500c52bc1a4956c560a32144fb27
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-04 03:00:54 +0000

    fast_start.txt for fast debugging.
    DO NOT CHECK THIS FILE IN. EVER.

--- trunk/source/IntroScene.py
+++ trunk/source/IntroScene.py
@@ -9,7 +9,56 @@ class IntroScene:
 		pass 
 	
 	def update(self):
-		self.next = PlayScene('ship_1', 8, 9, Context())
+		self.next = self.getStartScene()
 	
 	def render(self, screen, rc):
 		pass
+	
+	def getStartScene(self):
+		if os.path.exists('fast_start.txt'):
+			c = open('fast_start.txt', 'rt')
+			t = c.read().split('\n')
+			c.close()
+			i = 0
+			ignore = False
+			level = None
+			x = 2
+			y = 2
+			flags = []
+			for line in t:
+				line = trim(line.split('#')[0])
+				if len(line) > 0:
+					i += 1
+					if i == 1:
+						if line == 'IGNORE':
+							ignore = True
+							break
+						level = line
+					elif i == 2:
+						parts = line.split(',')
+						x = int(trim(parts[0]))
+						y = int(trim(parts[1]))
+					else:
+						flags.append(trim(line).lower())
+			
+			if not ignore:
+				c = Context()
+				for flag in flags:
+					if flag == 'gravity':
+						c.gravity = True
+					elif flag == 'volcano1':
+						c.volcanoA = True
+					elif flag == 'volcano2':
+						c.volcanoB = True
+					elif flag == 'volcano3':
+						c.volcanoC = True
+					elif flag == 'water1':
+						c.balloonA = True
+					elif flag == 'water2':
+						c.balloonB = True
+					elif flag == 'water3':
+						c.balloonC = True
+				
+				return PlayScene(level, x, y, c)
+				
+		return PlayScene('ship_1', 8, 9, Context())
