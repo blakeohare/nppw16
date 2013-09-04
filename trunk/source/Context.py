d85fa410c2e77f9d4c6b2d0d019c070dde137097
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-04 06:31:58 +0000

    Password encoder, decoder, and game over scene.

--- trunk/source/Context.py
+++ trunk/source/Context.py
@@ -1,8 +1,14 @@
+FULL_LIFE = 10
+START_LIFE = 2
+START_LIVES = 1
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 class Context:
+	
 	def __init__(self):
-		self.lifemeter = 10
-		self.lives = 3
+		self.lifemeter = START_LIFE
+		self.lives = START_LIVES
 		self.volcanoA = False
 		self.volcanoB = False
 		self.volcanoC = False
@@ -16,36 +22,117 @@ class Context:
 		self.transmission4 = False
 	
 	def convertToPassword(self):
-		password = 0
-		things = [
-			True,
-			self.gravity,
-			self.volcanoA,
-			not self.volcanoB,
-			self.volcanoC,
-			self.balloonA,
-			not self.balloonB,
-			self.balloonC]
-			
-		z = 1
-		for thing in things:
-			if thing:
-				password += 1 << z
-			z += 1
 		
-		output = [0, 0, 0, 0]
-		for i in range(4):
-			output[i] = password & 3
-			password = password >> 2
+		if self.balloonA or self.balloonB or self.balloonC:
+			count = 3
+		elif self.volcanoA or self.volcanoB or self.volcanoC:
+			count = 2
+		elif self.gravity:
+			count = 1
+		else:
+			count = 0
 		
+		password = ''
+		
+		if count == 1:
+			password = 'A'
+		elif count == 2:
+			password = 'B'
+		elif count == 3:
+			password = 'C'
+		else:
+			password = self.randomChar(ALPHABET[3:])
+		
+		if count >= 1:
+			if self.gravity:
+				password += self.randomChar(ALPHABET[:13])
+			else:
+				password += self.randomChar(ALPHABET[13:])
+		else:
+			password += self.randomChar(ALPHABET)
+		
+		if count >= 2:
+			value = self.volcanoA + (self.volcanoB * 2) + (self.volcanoC * 4)
+			letter = chr(ord('1') + value - 1)
+			password += letter
+		else:
+			password += self.randomChar('0123456789')
+		
+		if count >= 3:
+			value = self.balloonA + (self.balloonB * 2) + (self.balloonC * 4)
+			letter = chr(ord('A') + value - 1)
+			password += letter
+		else:
+			password += self.randomChar(ALPHABET)
+		
+		return password
+		
+		
+	# First letter:
+	# A - 2nd letter is password, 3rd and 4th are random
+	# B - 2nd and 3rd letter are password, 4th is random
+	# C - All 3 are used
+	# D+ - password is random, no state
+	
+	# Second letter when not random
+	# <= M - gravity is on
+	# >= N - gravity is off
+	
+	# Third letter when not random
+	# 1 - volcanos 1, 0, 0
+	# 2 - volcanos 0, 1, 0
+	# 3 - volcanos 1, 1, 0
+	# 4 - volcanos 0, 0, 1
+	# 5 - volcanos 1, 0, 1
+	# 6 - volcanos 0, 1, 1
+	# 7 - volcanos 1, 1, 1
+	# >= 8 - volcanos 0, 0, 0
+	
+	# Fourth letter when not random
+	# same pattern for volcanos, but for water, also uses letters instead of numbers
+	
+	def randomChar(self, characters):
+		return characters[int(random.random() * len(characters))]
+	
+	def enforceLetter(self, letter):
+		letter = letter.upper()
+		if ord(letter) < ord('A'):
+			return 'A'
+		if ord(letter) > ord('Z'):
+			return 'Z'
+		return letter
+	
+	def enforceNumber(self, char):
+		if ord(char) < ord('0'):
+			return '0'
+		if ord(char) > ord('9'):
+			return '9'
+		return char
+	
 	def convertFromPassword(self, password):
-		self.gravity = (password[0] & 2) != 0
-		self.volcanoA = (password[1] & 1) != 0
-		self.volcanoB = (password[1] & 2) == 0
-		self.volcanoC = (password[2] & 1) != 0
-		self.balloonA = (password[2] & 2) != 0
-		self.balloonB = (password[3] & 1) == 0
-		self.balloonC = (password[3] & 2) != 0
+		p1 = self.enforceLetter(password[0])
+		p2 = self.enforceLetter(password[1])
+		p3 = self.enforceLetter(password[2])
+		p4 = self.enforceLetter(password[3])
+		
+		if p1 in 'ABC':
+			if p2 <= ord('M'):
+				self.gravity = True
+		
+		if p1 in 'BC':
+			value = ord(p3) - ord('1') + 1
+			self.volcanoA = (value & 1) != 0
+			self.volcanoB = (value & 2) != 0
+			self.volcanoC = (value & 4) != 0
+		
+		if p1 == 'C':
+			value = ord(p4) - ord('A') + 1
+			self.balloonA = (value & 1) != 0
+			self.balloonB = (value & 2) != 0
+			self.balloonC = (value & 4) != 0
+		
+		self.lifemeter = START_LIFE
+		self.lives = START_LIVES
 		
 	def adjustHealth(self, scene, amount):
 		self.lifemeter += amount
---
+++ trunk/source/GameOverScene.py
@@ -0,0 +1,71 @@
+class GameOverScene:
+	def __init__(self, context):
+		self.next = self
+		self.flags = ''
+		self.context = context
+		self.counter = 0
+		self.index = 0
+		self.mode = 'prompt'
+		self.password = context.convertToPassword()
+		
+	def processInput(self, events, pressed):
+		if self.mode == 'prompt':
+			for event in events:
+				if event.down:
+					if event.action == 'up':
+						if self.index == 1:
+							self.index = 0
+					elif event.action == 'down':
+						if self.index == 0:
+							self.index = 1
+					elif event.action in ('A', 'B', 'start'):
+						self.mode = 'commit'
+						self.counter = 0
+	
+	def update(self):
+		self.counter += 1
+		if self.mode == 'commit':
+			if self.counter > 30:
+				if self.index == 0:
+					self.next = PlayScene('ship_1', 8, 9, self.context)
+				elif self.index == 1:
+					self.next = TitleScene()
+				else:
+					# what?
+					self.next = OpeningScene()
+		
+	
+	def render(self, screen, rc):
+		screen.fill((0, 0, 0))
+		show_selected = self.mode == 'prompt' or (rc % 2) == 0
+		gameOver = getText(WHITE, "GAME OVER")
+		optionA = getText(WHITE, "CONTINUE")
+		optionB = getText(WHITE, "END")
+		password = getText(WHITE, "PASSWORD:")
+		passwordValue = getText((128, 128, 128), self.password)
+		cursor = getText(WHITE, ">")
+		
+		left = 80
+		top = 96
+		
+		x = left
+		y = top
+		screen.blit(gameOver, (left, top))
+		
+		y += 16
+		if self.index == 0:
+			screen.blit(cursor, (x, y))
+		if show_selected or self.index == 1:
+			screen.blit(optionA, (x + 16, y))
+		
+		y += 16
+		if self.index == 1:
+			screen.blit(cursor, (x, y))
+		if show_selected or self.index == 0:
+			screen.blit(optionB, (x + 16, y))
+		
+		y += 48
+		x -= 32
+		screen.blit(password, (x, y))
+		x += password.get_width() + 8
+		screen.blit(passwordValue, (x, y))
