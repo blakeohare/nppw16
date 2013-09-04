cc0d8777f9406fd33d392bd6115849f9932dbda9
phiofpi <phiofpi@gmail.com>
2013-09-04 05:36:57 +0000

    added extant volcano files to Backgrounds.py

--- source/Backgrounds.py
+++ source/Backgrounds.py
@@ -9,7 +9,13 @@ CAVE_BG = [
 ]
 
 VOLCANO_BG = [
-	
+	'volcano_1_ascent',
+	'volcano_1_core',
+	'volcano_1_descent',
+	'lava_gate_2',
+	'lava_gate_2_active',
+	'lava_gate_3',
+	'lava_gate_3_active'
 ]
 
 STARS_BG = [
