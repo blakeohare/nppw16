55734eddbf44fe466971e611ad6e9b7118ed91e3
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-06 10:34:21 +0000

    Assign all maps to songs. Print warning message when game starts and a non overlay map does not have a song assigned. Assign fluffy tales to password screen.

--- trunk/source/JukeBox.py
+++ trunk/source/JukeBox.py
@@ -2,13 +2,77 @@ SONG_LOOKUP = {
 	# name : (file, should loop?)
 	'title': ('tensebranches', True),
 	'intro': ('open_skies', True),
-	'overworld': ('spacemachine', True),
-	'credits': ('credits', True)
+	'overworld': ('open_skies', True),
+	'ship': ('spacemachine', True),
+	'credits': ('credits', True),
+	'password': ('fluffytales', True),
+	'volcano': ('lunacy', True),
+	'bike': ('nopressure', True),
+	'water': ('squirrels_can_swim', True),
+	'gravity': ('deadlyacorns', True),
 }
 
 SONG_BY_MAP = {
-	'ship_1': 'overworld'
+	'ship_1': 'ship',
+	
+	'main': 'overworld',
+	'balloon_2_overland': 'overworld',
+	'balloon_3_overland': 'overworld',
+	'bird_overland': 'overworld',
+	'volcano_2_overland': 'overworld',
+	'volcano_3_overland': 'overworld',
+	
+	'bike_level': 'bike',
+	
+	'exit_water_phase_locked': 'water',
+	'exit_water_phase_unlocked': 'water',
+	
+	'grav_ascent': 'gravity',
+	'grav_core': 'gravity',
+	'grav_descent': 'gravity',
+	'grav_descent_secret': 'gravity',
+	'grav_gate': 'gravity',
+	
+	'lava_gate_2': 'volcano',
+	'lava_gate_2_active': 'volcano',
+	'lava_gate_3': 'volcano',
+	'lava_gate_3_active': 'volcano',
+	'volcano_1_ascent': 'volcano',
+	'volcano_1_core': 'volcano',
+	'volcano_1_descent': 'volcano',
+	'volcano_2_core': 'volcano',
+	'volcano_2_lower_ascent': 'volcano',
+	'volcano_2_lower_descent': 'volcano',
+	'volcano_2_upper_ascent': 'volcano',
+	'volcano_2_upper_descent': 'volcano',
+	'volcano_2_upper_secret': 'volcano',
+	'volcano_3_bottom': 'volcano',
+	'volcano_3_core': 'volcano',
+	'volcano_3_middle': 'volcano',
+	'volcano_3_secret': 'volcano',
+	'volcano_3_top': 'volcano',
+	
+	'water_gate_1_locked': 'water',
+	'water_gate_1_unlocked': 'water',
+	
+	'rope_test': 'gravity',
+	
+	'birdfeeder': 'gravity',
+	
+	'section_3_gate': 'gravity',
+	'section_3_gate_active': 'gravity',
 }
+
+for file in os.listdir('maps'):
+	if file.endswith('.map'):
+		if 'overlay' in file:
+			pass
+		else:
+			song = SONG_BY_MAP.get(file.split('.')[0])
+			if song == None:
+				print("ERROR: no song assigned to map file: " + file)
+
+
 
 class JukeBox:
 	def __init__(self):
