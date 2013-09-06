a6b6b96f7269cded355772307dc2e0b2a950f949
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@25a8915a-fec8-07ff-33d1-c7ad0c7badca>
2013-09-06 21:02:21 +0000

    Remove debug print statements.

--- trunk/source/SpriteRenderers/WaterPop.py
+++ trunk/source/SpriteRenderers/WaterPop.py
@@ -3,5 +3,4 @@ def SPRITE_renderWaterPop(sprite, scene, screen, offsetX, offsetY, arc):
 	
 	left = sprite.x + offsetX - 24
 	top = sprite.y + offsetY - 8
-	print("WORKS", left, top, sprite.x, sprite.y)
 	screen.blit(getImage('tiles/balloons/pop.png'), (left, top))
