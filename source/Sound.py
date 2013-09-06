5c013a27bbec9feaf1728fe0674b65f073f1e269
adrian.cline <adrian.cline@gmail.com>
2013-09-06 03:30:08 +0000

    Yay, a conflict, resolved.

--- source/Sound.py
+++ source/Sound.py
@@ -3,26 +3,25 @@
 # Otherwise, entry is a tuple of the filename (without extension, sfx/ is assumed) followed by a normalization ratio.
 
 SOUND_LOOKUP = {
-	'head_bonk': None, # fairly sublte sound as it will also happen a lot. Re-used when trying to move a menu cursor someplace where it cannot go.
-	'password_enter_digit': None, 
-	'bad_password': None, # sad fanfare: "DOO DOOoooo"
-	'sprinkle_lava_packet': None, # "chkhchkhchkchkh" no longer than 1 second
+	'head_bonk': ('head_bonk',1.0), # fairly sublte sound as it will also happen a lot. Re-used when trying to move a menu cursor someplace where it cannot go.
+	'password_enter_digit':('password_digit_enter',1.0), 
+	'bad_password': ('bad_password',1.0), # sad fanfare: "DOO DOOoooo"
+	'sprinkle_lava_packet': ('sprinkle_lava',1.0), # "chkhchkhchkchkh" no longer than 1 second
 	'screen_shaking': ('everythingfalls', 1.0), # done. perfect.
-	'fall_to_death': None, # fairly obvious
+	'fall_to_death': ('fall_to_death',1.0), # fairly obvious
 	'lava_rise': ('crash', 1.0),
-	'land_on_ground': None, # subtle sound as it will happen a lot
+	'land_on_ground': ('land_on_ground',0.5), # subtle sound as it will happen a lot
 	
 	# TODO(Blake): make sure these are hooked up in all menus
 	'menu_select': ('menuhigh', 6.0), # positive sound
 	'menu_move': ('menulow', 4.0), # subtle
 	
-	'low_health': None, # annoying as hell, 5 high pitched beeps in quick succession
-	'get_hit': None, # use megaman taking-damage sound as inspiration, if that's doable.
-	'raise_health': None, # dwoooOOO (but short)
+	'low_health': ('low_health',1.0), # annoying as hell, 5 high pitched beeps in quick succession
+	'get_hit': ('get_hit',1.0), # use megaman taking-damage sound as inspiration, if that's doable.
+	'raise_health': ('raise_health',1.0), # dwoooOOO (but short)
 	'jump': ('jump', 0.4), 
 	'swim': ('jump', 0.4), # maybe use a different sound?
-	'pause_sound': None, # "TEE KOO TEE KOO!"
-	
+	'pause_sound': ('pause_sound',1.0), # "TEE KOO TEE KOO!"
 	# TODO(Blake): The following are not hooked in by the code yet
 	'lava_roast': None, # player dies by lava
 	'enemy_dies': None, # after shooting it
