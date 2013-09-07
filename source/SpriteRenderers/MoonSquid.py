11668538d057a81dbfb5e12647bbf2ad258916fe
Blake O'Hare <blakeohare@users.noreply.github.com>
2013-09-07 08:40:18 +0000

    MOON SQUIDS!

--- source/SpriteRenderers/MoonSquid.py
+++ source/SpriteRenderers/MoonSquid.py
@@ -1,7 +1,7 @@
 def SPRITE_renderMoonSquid(sprite, scene, screen, offsetX, offsetY, arc):
 	left = offsetX + sprite.x - 8
 	top = offsetY + sprite.y - 8
-	
+	arc = arc // 3
 	img = getImage('sprites/moonsquid_1.png' if ((arc & 1) == 0) else 'sprites/moonsquid_2.png')
 	screen.blit(img, (left, top))
 	
