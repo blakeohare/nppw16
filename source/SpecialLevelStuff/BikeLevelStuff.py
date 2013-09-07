213804a0e0b605a8a5b092e7c2faed261f97c19e
Blake O'Hare <blakeohare@users.noreply.github.com>
2013-09-07 19:54:00 +0000

    A much better-looking seed scattering algorithm

--- source/SpecialLevelStuff/BikeLevelStuff.py
+++ source/SpecialLevelStuff/BikeLevelStuff.py
@@ -3,27 +3,42 @@ class BikeLevelStuff(SpecialLevelStuff):
 		SpecialLevelStuff.__init__(self, scene)
 		self.hasUpdate = True
 		self.hasRender = True
+		self.particles = []
+		self.counter = 0
 		
 	def update(self):
-		
+		self.counter += 1
 		scene = self.scene
 		player = scene.player
 		if player.x > (scene.cols - 1) * 16: # one less because you can't technically go off screen
 			scene.next = VictorySequenceScene(scene)
+		
+		if self.counter % 2 == 0:
+			pt = StemCell()
+			pt.x = player.x
+			pt.y = player.y
+			pt.vx = random.random() * 16 - 8
+			pt.vy = random.random() * 6 - 5
+			self.particles.append(pt)
+		
+		for pt in self.particles:
+			pt.x += pt.vx
+			pt.y += pt.vy
+			pt.vy += 2
 	
 	def render(self, screen, rc, offsetX, offsetY):
 		p = self.scene.player
 		px = p.x + offsetX
 		py = p.y + offsetY
-		i = 0
-		while i < 30:
-			y = py + i * 12 + (rc % 12)
-			x = px + int(math.sin(rc * 2 * 3.14159 / 60) * 24 * i) * (-1 if (i % 2 == 0) else 1)
-			PDR(screen, (255, 255, 128), pygame.Rect(x, y, 4, 4))
-			i += 1
+		
+		for pt in self.particles:
+			PDR(screen, (255, 255, 128), pygame.Rect(pt.x + offsetX, pt.y + offsetY, 3, 3))
+		
+		if len(self.particles) > 40:
+			self.particles = self.particles[1:]
 		
 		
-		screen.blit(getText(WHITE, "Don't worry, I'll redo the"), (0, 8))
-		screen.blit(getText(WHITE, "scatter pattern before"), (0, 16))
-		screen.blit(getText(WHITE, "we turn it in."), (0, 24))
-		screen.blit(getText(WHITE, "              --Blake"), (0, 32))
+		#screen.blit(getText(WHITE, "Don't worry, I'll redo the"), (0, 8))
+		#screen.blit(getText(WHITE, "scatter pattern before"), (0, 16))
+		#screen.blit(getText(WHITE, "we turn it in."), (0, 24))
+		#screen.blit(getText(WHITE, "              --Blake"), (0, 32))
